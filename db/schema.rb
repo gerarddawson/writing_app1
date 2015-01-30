12a799d4c2096785ac95bc7ff120f677fccea078
Gerard Dawson <gerard.dawson1@gmail.com>
2015-01-29 20:10:04 -0500

    ran associations between posts and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130002236) do
+ActiveRecord::Schema.define(version: 20150130010506) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20150130002236) do
     t.text     "next"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
